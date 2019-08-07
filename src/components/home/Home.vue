4ad30e2d03ea7a030513dfc6c5095845516a7ee7
小狐狸 <862526226@qq.com>
2019-08-07 18:08:29 +0800

    axios

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,38 +1,54 @@
 <template>
-    <div class="home">
-        <home-header></home-header>
-        <home-swiper></home-swiper>
-        <home-icons></home-icons>
-        <home-weekSearch></home-weekSearch>
-        <home-recommend></home-recommend>
-        <home-weekgloging></home-weekgloging>
-    </div>
+  <div class="home">
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-weekSearch></home-weekSearch>
+    <home-recommend></home-recommend>
+    <home-weekgloging></home-weekgloging>
+  </div>
 </template>
 
 <script>
-import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
-import HomeIcons from './components/Icons'
-import HomeWeekSearch from './components/WeekSearch'
-import HomeRecommend from './components/Recommend'
-import HomeWeekgloging from './components/Weekendgoging'
+import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import HomeIcons from "./components/Icons";
+import HomeWeekSearch from "./components/WeekSearch";
+import HomeRecommend from "./components/Recommend";
+import HomeWeekgloging from "./components/Weekendgoging";
+import axios from "axios";
 export default {
-    name: 'Home',
-    components: {
-        HomeHeader,
-        HomeSwiper,
-        HomeIcons,
-        HomeRecommend,
-        HomeWeekSearch,
-        HomeWeekgloging
+  name: "Home",
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekSearch,
+    HomeWeekgloging
+  },
+  mounted() {
+    this.getHomeInfo();
+  },
+  methods: {
+    getHomeInfo() {
+      axios
+        .get("/api/index.json")
+        .then(this.getHomeInfoSucc)
+        .catch(function(error) {
+          console.log(error);
+        });
     },
-}
+    getHomeInfoSucc(res) {
+      console.log(res);
+    }
+  }
+};
 </script>
 
 <style>
 .home {
- background-color: #f5f5f5;
+  background-color: #f5f5f5;
 }
-    
 </style>
 
