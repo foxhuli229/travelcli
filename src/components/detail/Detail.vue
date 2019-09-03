e74bd1d328dd85211367f31a78fec23d9344ce3a
foxhuli229 <862526226@qq.com>
2019-09-03 23:28:52 +0800

    详情页header渐隐渐现样式

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
       <deatil-banner></deatil-banner>
+      <deatil-header></deatil-header>
+       <div class="conent"></div>
   </div>
 </template>
 
 <script>
 import DeatilBanner from './components/Banner'
+import DeatilHeader from './components/Header'
 export default {
     name: 'Detail',
     components: {
-        deatilBanner: DeatilBanner
+        deatilBanner: DeatilBanner,
+        DeatilHeader: DeatilHeader
     }
 }
 </script>
 
 <style lang="stylus" scoped>
+.conent {
+  height 50rem;
+}
 
 </style>
